b1e1e23f2cc493d255f368e764562a16b4129faa
Priyanshu4 <agpri123@gmail.com>
2023-02-11 14:01:15 -0500

    Added otis_api code for elevator_status

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import otisapi
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
-
+    data = otisapi.elevator_status("unit_001", "USA", "001", "001")
+    return str(data)
 
 app.run(host="0.0.0.0", port=5000)
