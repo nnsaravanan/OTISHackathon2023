4e9f5871ebbdda671b456c5b80079a842e315f3a
Priyanshu4 <agpri123@gmail.com>
2023-02-11 14:06:38 -0500

    app now renders index.html

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 import otisapi
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    data = otisapi.elevator_status("unit_001", "USA", "001", "001")
-    return str(data)
+    return render_template("index.html")
 
 app.run(host="0.0.0.0", port=5000)
